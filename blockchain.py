ea5bf0ebbac7475f792e188b7a7e234bf5983b15
KimJunHui <junhui820@gmail.com>
2018-10-16 22:53:49 +0900

    Modified blockchain.

--- blockchain.py
+++ blockchain.py
@@ -2,33 +2,30 @@ from block import *
 from transaction import *
 from leader_adapter import *
 from node_adapter import *
+import pymongo
 
 
 class Blockchain:
-    def __init__(self):
-        self.chain = []
+    def __init__(self, ip, port, db, chain_col, state_col, leader):
+        self.my_ip = ip
+
+        # mongo db connection
+        self.conn = pymongo.MongoClient(ip, port)
+        self.db = self.conn.get_database(db)
+
+        # block db
+        self.chain = self.db.get_collection(chain_col)
+
+        # state db
+        self.state = self.db.get_collection(state_col)
+
         self.tx_list = []
-        self.leader = ""
-
-    def new_transaction(self):
-        # tx type matching
-        if ():
-            pass
-        elif ():
-            pass
-        elif ():
-            pass
-        elif ():
-            pass
-        else:
-            pass
-
-        # save tx
-        # self.tx_list.append()
+        self.leader = leader
 
     def send_transaction(self):
         for tx in self.tx_list:
-            send_tx_msg(tx)
+            tx_json = tx.to_json()
+            send_tx_msg(tx_json, self.my_ip)
 
         self.tx_list.clear()
 
@@ -36,6 +33,21 @@ class Blockchain:
         self.tx_list.append(tx)
 
     def save_block(self, block):
-        # receive a block from leader
+        self.chain.insert(block)
+        self.update_state(block)
 
-        self.chain.append(block)
+    def update_state(self, block):
+        state = self.state
+
+        for tx in block.transactions:
+            if type(tx) == "":
+                state.update()
+            elif type(tx) == "":
+                state.update()
+            elif type(tx) == "":
+                state.update()
+            else:
+                print("[Error] Not defined transaction!")
+                return
+
+        self.state = state
