43beecd41c0a0eb8d08583030dcc899fc09bc462
KimJunHui <junhui820@gmail.com>
2018-10-28 18:20:11 +0900

    Refactoried leader_adapter.py.

--- leader_adapter.py
+++ leader_adapter.py
@@ -2,25 +2,25 @@ import requests
 from flask import request
 
 from block import *
-from transaction import new_transaction
+from transaction import *
 
 
 def receive_tx_msg(json_data):
     # http로 tx 받음
-    tx = new_transaction(json_data)
-    print(tx)
+    tx = json_to_tx(json_data)
+    print("Receive tx msg - ", tx)
     return tx
 
 
 def propose_block(last_block, tx_list, my_address):
-    b = new_block(last_block, tx_list, my_address)
-    block_json = b.to_json()
+    b = create_block(last_block, tx_list, my_address)
+    block_json = block_to_json(b)
 
     # b를 http 전송
     dst = "http://" + my_address + ":4444" + "/block"
 
     requests.post(url=dst, json=block_json)
-    pass
+    return b
 
 
 def broadcast_next_leader(next_leader, my_address):
