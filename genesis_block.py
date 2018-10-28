c532f6d7df4fb5ceaf7f24c2804f966894a8eb50
KimJunHui <junhui820@gmail.com>
2018-10-28 17:48:46 +0900

    Refactoried genesis_block.py.

--- genesis_block.py
+++ genesis_block.py
@@ -1,9 +1,13 @@
 import datetime as date
-from block import Block
+from block import *
 
 
 # Normally genesis block is JSON file
-def create_genesis_block():
-    # todo : how first transaction?
-    # todo : is a value of constructor IP?
-    return Block("0", str(date.datetime.now()), "Genesis Block", "0", "myIP")
+def create_genesis_block(ip):
+    index = 0
+    timestamp = date.datetime.now()
+    transactions = [{}]
+    prev_hash = "no_hash"
+    this_hash = hash_block(index, timestamp, transactions, prev_hash, ip)
+
+    return new_block(0, str(date.datetime.now()), [{}], "no_hash", ip, this_hash)
