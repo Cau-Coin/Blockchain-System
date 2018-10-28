e90046d192dbcea5624da0c281ae4429db286ef6
KimJunHui <junhui820@gmail.com>
2018-10-28 18:32:22 +0900

    Fixed that userid is not declared.

--- transaction.py
+++ transaction.py
@@ -2,70 +2,73 @@ import json
 import uuid
 
 
-def new_evaluate_tx(tx_id, tx_maker, timestamp, evaluate_id, dept, grade, semester, subject, takeyear, score,
-                    review):
+def new_evaluate_tx(tx_id, tx_maker, timestamp, evaluate_id, user_id, dept, grade, semester, subject, takeyear,
+                    evaluate, review):
     eval_tx_data = {
         "tx_id": tx_id,
         "tx_maker": tx_maker,
         "timestamp": timestamp,
         "tx_type": "evaluate",
         "evaluate_id": evaluate_id,
+        "user_id": user_id,
         "dept": dept,
         "grade": grade,
         "semester": semester,
         "subject": subject,
         "takeyear": takeyear,
-        "score": score,
+        "evaluate": evaluate,
         "review": review
     }
 
     return eval_tx_data
 
 
-def create_evaluate_tx(tx_maker, timestamp, dept, grade, semester, subject, takeyear, score, review):
+def create_evaluate_tx(tx_maker, timestamp, user_id, dept, grade, semester, subject, takeyear, evaluate, review):
     tx_id = str(uuid.uuid4())
     evaluate_id = str(uuid.uuid4())
 
-    return new_evaluate_tx(tx_id, tx_maker, timestamp, evaluate_id, dept, grade, semester, subject, takeyear, score,
-                           review)
+    return new_evaluate_tx(tx_id, tx_maker, timestamp, evaluate_id, user_id, dept, grade, semester, subject, takeyear,
+                           evaluate, review)
 
 
-def new_comment_tx(tx_id, tx_maker, timestamp, evaluate_id, comment):
+def new_comment_tx(tx_id, tx_maker, timestamp, evaluate_id, user_id, comment):
     comment_tx_data = {
         "tx_id": tx_id,
         "tx_maker": tx_maker,
         "timestamp": timestamp,
         "tx_type": "comment",
         "evaluate_id": evaluate_id,
+        "user_id": user_id,
         "comment": comment,
     }
 
     return comment_tx_data
 
 
-def create_comment_tx(tx_maker, timestamp, evaluate_id, comment):
+def create_comment_tx(tx_maker, timestamp, evaluate_id, user_id, comment):
     tx_id = str(uuid.uuid4())
 
-    return new_comment_tx(tx_id, tx_maker, timestamp, evaluate_id, comment)
+    return new_comment_tx(tx_id, tx_maker, timestamp, evaluate_id, user_id, comment)
 
 
-def new_score_tx(tx_id, tx_maker, timestamp, evaluate_id, score):
+def new_score_tx(tx_id, tx_maker, timestamp, evaluate_id, user_id, score):
     score_tx_data = {
         "tx_id": tx_id,
         "tx_maker": tx_maker,
         "timestamp": timestamp,
         "tx_type": "score",
         "evaluate_id": evaluate_id,
+        "user_id": user_id,
         "score": score,
     }
 
     return score_tx_data
 
 
-def create_score_tx(tx_maker, timestamp, evaluate_id, score):
+def create_score_tx(tx_maker, timestamp, evaluate_id, user_id, score):
     tx_id = str(uuid.uuid4())
 
-    return new_score_tx(tx_id, tx_maker, timestamp, evaluate_id, score)
+    return new_score_tx(tx_id, tx_maker, timestamp, evaluate_id, user_id, score)
 
 
 def tx_to_json(tx):
@@ -80,13 +83,15 @@ def create_tx(my_ip, json_input):
     data_input = json.loads(json_input)
 
     if data_input["tx_type"] == "evaluate":
-        return create_evaluate_tx(my_ip, data_input["timestamp"], data_input["dept"], data_input["grade"],
-                                  data_input["semester"], data_input["subject"], data_input["takeyear"],
-                                  data_input["score"], data_input["review"])
+        return create_evaluate_tx(my_ip, data_input["timestamp"], data_input["userid"], data_input["dept"],
+                                  data_input["grade"], data_input["semester"], data_input["subject"],
+                                  data_input["takeyear"], data_input["evaluate"], data_input["review"])
     elif data_input["tx_type"] == "comment":
-        return create_comment_tx(my_ip, data_input["timestamp"], data_input["evaluate_id"], data_input["comment"])
+        return create_comment_tx(my_ip, data_input["timestamp"], data_input["evaluateid"], data_input["userid"],
+                                 data_input["comment"])
     elif data_input["tx_type"] == "score":
-        return create_score_tx(my_ip, data_input["timestamp"], data_input["evaluate_id"], data_input["score"])
+        return create_score_tx(my_ip, data_input["timestamp"], data_input["evaluateid"], data_input["userid"],
+                               data_input["score"])
     else:
         print("Create tx is failed! - tx_type mismatched")
         return {}
